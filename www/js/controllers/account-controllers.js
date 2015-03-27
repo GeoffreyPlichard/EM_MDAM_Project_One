f22ea606288604492e108e2d46fa8b71d6f31f7c
Geoffrey P <geoffreyplichard@gmail.com>
2015-03-27 13:07:00 +0100

    reorganize into folders for controllers & services

--- www/js/controllers/account-controllers.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module('user', []);
-app.controller('usersCtrl', function($scope, $http) {
-    $http.get("../js/data.json")
-    .success(function(response) {$scope.names = response;});
-});
