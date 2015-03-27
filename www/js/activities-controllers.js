b9634d2d7c88d2ebd24bc6e172b60a4badf9636b
Mzkr <alexissourbet@gmail.com>
2015-03-27 10:50:29 +0100

    loader categories

--- www/js/activities-controllers.js
+++ www/js/activities-controllers.js
@@ -21,14 +21,7 @@ angular.module('activities.controllers', [])
 			}
 		}
 		var onSuccess = function(position) {
-			console.log('Latitude: '          + position.coords.latitude          + '\n' +
-		          'Longitude: '         + position.coords.longitude         + '\n' +
-		          'Altitude: '          + position.coords.altitude          + '\n' +
-		          'Accuracy: '          + position.coords.accuracy          + '\n' +
-		          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-		          'Heading: '           + position.coords.heading           + '\n' +
-		          'Speed: '             + position.coords.speed             + '\n' +
-		          'Timestamp: '         + position.timestamp                + '\n');
+			
 			ActivitiesService.get_geo_equipments(equipment_ids, position.coords.latitude, position.coords.longitude, FavoriteService.getRay(), function(res){
 				$scope.equipments = res.data;
 				$scope.selections =SelectionService.getAll();
--- www/js/profil-controller.js
+++ www/js/profil-controller.js
@@ -6,7 +6,7 @@
 angular.module('profil.controller', [])
 
 .controller('ProfilCtrl', function($scope, $rootScope, $ionicLoading){
-
+    alert($rootScope.data_picture_url);
     if($rootScope.accountExist === undefined || $rootScope.accountExist === "" || $rootScope.accountExist === null){
         $rootScope.accountExist = JSON.parse(localStorage.getItem("user.ownData"));
     }
